4a8c8d9f0c86d7472549392d73c8d2f054ebeb36
Maria Martinkous <marticaida@gmail.com>
2023-03-16 01:48:33 -0500

    basic task layout and rendering

--- src/dom_build.js
+++ src/dom_build.js
@@ -1,3 +1,18 @@
-export default function () {
+export default function (task) {
   const todosContainer = document.getElementById('todos')
+
+  const taskElement = document.createElement('div')
+  const content = document.createElement('div')
+  const details = document.createElement('div')
+  const dueDate = document.createElement('div')
+  const priority = document.createElement('div')
+  content.innerHTML = task.content
+  details.innerHTML = task.details
+  dueDate.innerHTML = task.dueDate
+  priority.innerHTML = task.priority
+  taskElement.appendChild(content)
+  taskElement.appendChild(details)
+  taskElement.appendChild(dueDate)
+  taskElement.appendChild(priority)
+  todosContainer.appendChild(taskElement)
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import './style.css'
 import renderPage from './dom_build'
 
-renderPage()
+const taskActions = {
+  changeContent (newContent) {
+    return (this.content = newContent)
+  },
+  changeDetails (newDetails) {
+    return (this.details = newDetails)
+  },
+  changeDueDate (newDate) {
+    return (this.dueDate = newDate)
+  },
+  changePriority (newPriority) {
+    return (this.priority = newPriority)
+  }
+}
 
+function createTask (content, details, dueDate, priority) {
+  const task = Object.create(taskActions)
+  task.content = content
+  task.details = details
+  task.dueDate = dueDate
+  task.priority = priority
+  return task
+}
 
+const taskOne = createTask('do this', 'like so', 'on the 1st', 'high')
+
+// taskOne.changeDueDate = taskActions.changeDueDate
+console.log(taskOne)
+taskOne.changeDueDate('new Date')
+taskOne.changeContent('remember about this')
+taskOne.changePriority('low')
+console.log(taskOne)
+
+renderPage(taskOne)
