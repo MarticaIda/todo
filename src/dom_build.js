2e05ff9cea98a424745d6e3ec6dffe2c802306f0
Maria Martinkous <marticaida@gmail.com>
2023-04-03 14:34:22 -0500

    add edit of all fields

--- src/dom_build.js
+++ src/dom_build.js
@@ -16,21 +16,44 @@ export default function generateTable () {
       data.textContent = task[entry]
       row.appendChild(data)
       const input = document.createElement('input')
+      const select = document.createElement('select')
+      select.id = 'priority'
+      const options = ['yesterday', 'high', 'low', 'no rush']
+      for (let i = 0; i < options.length; i++) {
+        const option = document.createElement('option')
+        option.value = options[i]
+        option.text = options[i]
+        select.appendChild(option)
+      }
       data.addEventListener('click', addInput)
       input.addEventListener('change', saveInput)
+      select.addEventListener('change', saveSelect)
 
       function addInput (event) {
         if (event.target !== this) {
           return
         }
-        input.setAttribute('type', 'text')
-        input.setAttribute('value', task[entry])
         data.textContent = ''
-        data.appendChild(input)
+        if (data.classList.contains('dueDate')) {
+          input.setAttribute('type', 'date')
+          input.setAttribute('value', '')
+          data.appendChild(input)
+        } else if (data.classList.contains('priority')) {
+          select.setAttribute('value', task[entry])
+          data.appendChild(select)
+        } else {
+          input.setAttribute('type', 'text')
+          input.setAttribute('value', task[entry])
+          data.appendChild(input)
+        }
       }
 
-      function saveInput (event) {
+      function saveInput () {
         task[entry] = input.value
+        data.textContent = task[entry]
+      }
+      function saveSelect () {
+        task[entry] = select.value
         data.textContent = task[entry]
       }
     })
