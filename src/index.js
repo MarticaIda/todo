66c3536d9b6ad073c0b75ca31a0a86f77b883761
Maria Martinkous <marticaida@gmail.com>
2023-04-17 08:10:34 -0500

    refactor project, todo inputs, table rendering with complete and incomplete

--- src/index.js
+++ src/index.js
@@ -1,46 +1,36 @@
-// add date added property?
 // make todos sortable by dueDate and priority
-// view all todos in each project (probably just the title and duedate… perhaps changing color for different priorities)
 // expand a single todo to see/edit its details
-// setting todos as complete
-// change date format to mm/dd/yyyy
 
 import './style.css'
-import { projectList, createTodo, formatDate } from './createTodo'
+import {
+  projectList,
+  createTodo,
+  formatDate,
+  completedTodos
+} from './createTodo'
 import BinImg from './recycle-bin.png'
 import EditImg from './edit.png'
 
-const projectElement = document.getElementById('project')
-const projectBar = document.querySelector('ul')
-const projectHeader = document.querySelector('h1')
+export const incompleteTable = document.querySelector('.incomplete')
+const completeTable = document.querySelector('.complete')
 const form = document.querySelector('form')
 const openBtn = document.querySelector('#btnOpen')
 const modal = document.querySelector('#modal')
 const closeBtn = document.querySelector('#btnClose')
-const addProjectBtn = document.getElementById('addProject')
 const nameEl = document.getElementById('task')
 const detailsEl = document.getElementById('details')
 const dueDateEl = document.getElementById('dueDate')
 const priorityEl = document.getElementById('priority')
+const nav = document.querySelector('nav')
+const projectElement = document.getElementById('project')
+const projectBar = document.querySelector('ul')
+const projectHeader = document.querySelector('h1')
+const addProjectBtn = document.getElementById('addProject')
 const viewAllTodosBtn = document.querySelector('#viewAllBtn')
-let projectName
+export const incompleteTBody = document.getElementById('incompleteTBody')
+const completeTBody = document.getElementById('completeTBody')
 
-// new project input
-addProjectBtn.addEventListener('click', (e) => {
-  e.preventDefault()
-  const project = projectElement.value
-  if (project === '') {
-    alert('Please enter a project name')
-    return
-  }
-  if (projectList[project]) {
-    alert('Project already exists')
-  } else {
-    projectList[project] = []
-  }
-  generateProjectBar(projectList)
-  projectElement.value = ''
-})
+let projectName
 
 modal.style.display = 'none'
 
@@ -58,7 +48,7 @@ const todoInput = () => {
     const priority = priorityEl.value
 
     if (name === '') {
-      alert('Please enter a task name')
+      displayErrorMessage('Please enter a task name')
       return
     }
     createTodo(name, details, dueDate, priority, false, projectName)
@@ -73,71 +63,155 @@ const todoInput = () => {
 }
 todoInput()
 
+// new project input
+addProjectBtn.addEventListener('click', handleProjectInput)
+
+function handleProjectInput (event) {
+  event.preventDefault()
+  const project = projectElement.value.trim()
+  if (!project) {
+    displayErrorMessage('Please enter a project name')
+    return
+  }
+  if (projectList[project]) {
+    displayErrorMessage('Project already exists')
+    return
+  }
+  projectList[project] = []
+  generateProjectList(projectList)
+  projectElement.value = ''
+}
+
 // render project list and project selection
-function generateProjectBar () {
+function generateProjectList () {
   projectBar.textContent = ''
+
   for (const project in projectList) {
-    const projectContainer = document.createElement('div')
-    projectContainer.setAttribute('class', 'projectContainer')
     const listItem = document.createElement('li')
     listItem.textContent = project
-    const editIcon = new Image()
-    editIcon.src = EditImg
-    projectContainer.appendChild(listItem)
-    projectContainer.appendChild(editIcon)
-    projectBar.appendChild(projectContainer)
-    const input = document.createElement('input')
     listItem.addEventListener('click', () => {
       projectHeader.textContent = project
       projectName = project
-      generateTable(projectList[project])
+      generateTable(projectList[project], incompleteTable, incompleteTBody)
     })
-    editIcon.addEventListener('click', () => {
-      input.setAttribute('type', 'text')
-      input.setAttribute('value', project)
-      listItem.textContent = ''
-      listItem.appendChild(input)
-    })
-    input.addEventListener('change', () => {
-      const newProjectName = input.value
-      if (newProjectName === '') {
-        alert('Please enter a project name')
-        return
-      }
-      if (projectList[newProjectName]) {
-        alert('Project already exists')
-      } else {
-        projectList[newProjectName] = projectList[project]
-        delete projectList[project]
-        generateProjectBar()
-        generateTable(projectList[newProjectName])
-      }
-    })
-    const deleteIcon = new Image()
-    deleteIcon.src = BinImg
+
+    const editIcon = createIcon(EditImg)
+    editIcon.addEventListener('click', () =>
+      handleEditProject(project, listItem)
+    )
+
+    const deleteIcon = createIcon(BinImg)
+    deleteIcon.addEventListener('click', () => handleDeleteProject(project))
+
+    const projectContainer = document.createElement('div')
+    projectContainer.setAttribute('class', 'projectContainer')
+    projectContainer.appendChild(listItem)
+    projectContainer.appendChild(editIcon)
     projectContainer.appendChild(deleteIcon)
-    deleteIcon.addEventListener('click', () => {
-      const confirmed = confirm('Are you sure you want to delete this project?')
-      if (confirmed) {
-        delete projectList[project]
-        generateProjectBar()
-      }
-    })
+    projectBar.appendChild(projectContainer)
   }
 }
 
-const tbody = document.querySelector('tbody')
+function handleEditProject (project, listItem) {
+  const input = document.createElement('input')
+  input.setAttribute('type', 'text')
+  input.setAttribute('value', project)
+  listItem.textContent = ''
+  listItem.appendChild(input)
+
+  input.addEventListener('change', () => {
+    const newProjectName = input.value.trim()
+    if (!newProjectName) {
+      displayErrorMessage('Please enter a project name')
+      return
+    }
+    if (projectList[newProjectName]) {
+      displayErrorMessage('Project already exists')
+      input.focus()
+      return
+    }
+    projectList[newProjectName] = projectList[project]
+    delete projectList[project]
+    generateProjectList()
+    generateTable(projectList[newProjectName], incompleteTable, incompleteTBody)
+  })
+}
+
+function handleDeleteProject (project) {
+  const confirmed = confirm('Are you sure you want to delete this project?')
+  if (confirmed) {
+    delete projectList[project]
+    generateProjectList()
+  }
+}
+
+function createIcon (src) {
+  const icon = new Image()
+  icon.src = src
+  return icon
+}
+
+function displayErrorMessage (message) {
+  const error = document.createElement('div')
+  error.textContent = message
+  error.classList.add('error')
+  nav.appendChild(error)
+
+  setTimeout(() => {
+    error.remove()
+  }, 3000)
+}
+
+// const tbody = document.querySelector('tbody')
 
 // rendering todos in a table
-export function generateTable (project) {
+export function generateTable (project, table, tbody) {
+  table.style.display = 'block'
   tbody.textContent = ''
   for (const todo of project) {
-    domBuild(todo)
+    if (project === completedTodos) {
+      displayCompletedRow(todo, tbody)
+    } else {
+      displayRegularRow(todo, tbody)
+    }
   }
 }
 
-// general table rendering logic
-const domBuild = (todo) => {
+// render completed table
+function displayCompletedRow (todo, tbody) {
+  const row = document.createElement('tr')
+  Object.keys(todo).forEach((entry) => {
+    const data = document.createElement('td')
+    data.setAttribute('class', entry)
+    data.textContent = todo[entry]
+    row.appendChild(data)
+    if (data.classList.contains('toDelete')) {
+      addDeleteInput(data, todo, entry, completedTodos)
+    }
+  })
+  tbody.appendChild(row)
+}
+
+// render incomplete table
+function displayRegularRow (todo, tbody) {
+  const row = createRow(todo)
+  const completeTodo = row.insertCell(-1)
+  const input = document.createElement('input')
+  input.setAttribute('class', 'completeTodo')
+  input.setAttribute('type', 'checkbox')
+  completeTodo.appendChild(input)
+  input.addEventListener('change', () => {
+    if (input.checked) {
+      completedTodos.push(todo)
+      generateTable(completedTodos, completeTable, completeTBody)
+      todo.toDelete = true
+      deleteTodo(projectList)
+    }
+  })
+  tbody.appendChild(row)
+}
+
+function createRow (todo) {
   const row = document.createElement('tr')
 
   Object.keys(todo).forEach((entry) => {
@@ -145,89 +219,105 @@ const domBuild = (todo) => {
     data.setAttribute('class', entry)
     data.textContent = todo[entry]
     row.appendChild(data)
-    const input = document.createElement('input')
-    const select = document.createElement('select')
-    select.id = 'priority'
-    const options = ['yesterday', 'high', 'low', 'no rush']
-    for (let i = 0; i < options.length; i++) {
-      const option = document.createElement('option')
-      option.value = options[i]
-      option.text = options[i]
-      select.appendChild(option)
-    }
-    const deleteIcon = new Image()
-    deleteIcon.src = BinImg
+
     if (data.classList.contains('toDelete')) {
-      data.textContent = ''
-      data.appendChild(deleteIcon)
+      addDeleteInput(data, todo, entry, projectList)
+    } else if (data.classList.contains('dueDate')) {
+      addInput(data, todo, entry, 'date')
+    } else if (data.classList.contains('priority')) {
+      const select = createSelect(todo[entry])
+      addInput(data, todo, entry, select)
+    } else {
+      addInput(data, todo, entry, 'text')
     }
-    data.addEventListener('click', addInput)
-    input.addEventListener('change', saveInput)
-    select.addEventListener('change', saveSelect)
-    deleteIcon.addEventListener('click', addInput)
+  })
+  return row
+}
 
-    function addInput (event) {
-      if (event.target !== this) {
-        return
-      }
-      data.textContent = ''
-      if (data.classList.contains('dueDate')) {
-        input.setAttribute('type', 'date')
-        input.setAttribute('value', '')
-        data.appendChild(input)
-      } else if (data.classList.contains('priority')) {
-        select.setAttribute('value', todo[entry])
-        data.appendChild(select)
-      } else if (data.classList.contains('toDelete')) {
-        todo[entry] = true
-        deleteTodo()
-      } else {
-        input.setAttribute('type', 'text')
-        input.setAttribute('value', todo[entry])
-        data.appendChild(input)
-      }
-    }
+function addDeleteInput (data, todo, entry, projects) {
+  const deleteIcon = new Image()
+  deleteIcon.src = BinImg
+  data.textContent = ''
+  data.appendChild(deleteIcon)
+  deleteIcon.addEventListener('click', () => {
+    console.log(projects)
+    todo[entry] = true
+    deleteTodo(projects)
+  })
+}
 
-    function saveInput () {
-      if (data.classList.contains('dueDate')) {
-        const date = formatDate(input.value)
-        todo[entry] = date
-        data.textContent = todo[entry]
-      } else {
-        todo[entry] = input.value
-        data.textContent = todo[entry]
-      }
-    }
-    function saveSelect () {
-      todo[entry] = select.value
+function addInput (data, todo, entry, type) {
+  const input = document.createElement('input')
+  input.setAttribute('type', type)
+  input.setAttribute('value', todo[entry])
+  data.addEventListener('click', () => {
+    data.textContent = ''
+    data.appendChild(input)
+    input.focus()
+  })
+  input.addEventListener('change', () => {
+    if (type === 'date') {
+      const date = formatDate(input.value)
+      todo[entry] = date
+      data.textContent = todo[entry]
+    } else {
+      todo[entry] = input.value
       data.textContent = todo[entry]
     }
   })
-  tbody.appendChild(row)
+}
+
+function createSelect (selectedValue) {
+  const select = document.createElement('select')
+  select.id = 'priority'
+  const options = ['yesterday', 'high', 'low', 'no rush']
+  for (let i = 0; i < options.length; i++) {
+    const option = document.createElement('option')
+    option.value = options[i]
+    option.text = options[i]
+    if (selectedValue === options[i]) {
+      option.setAttribute('selected', 'selected')
+    }
+    select.appendChild(option)
+  }
+  return select
 }
 
 // delete todo and render table
-function deleteTodo () {
-  for (const project in projectList) {
-    const todos = projectList[project]
-    for (let i = 0; i < todos.length; i++) {
-      const todo = todos[i]
+function deleteTodo (projects) {
+  if (projects === completedTodos) {
+    console.log(projects)
+    for (let i = 0; i < projects.length; i++) {
+      const todo = projects[i]
       if (todo.toDelete) {
-        todos.splice(i, 1)
+        projects.splice(i, 1)
         i--
       }
     }
-    if (projectHeader.textContent === 'All todos') {
-      generateTable(getAllTodos())
-    } else {
-      generateTable(projectList[project])
+    generateTable(completedTodos, completeTable, completeTBody)
+  } else {
+    console.log(projects)
+    for (const project in projects) {
+      const todos = projects[project]
+      for (let i = 0; i < todos.length; i++) {
+        const todo = todos[i]
+        if (todo.toDelete) {
+          todos.splice(i, 1)
+          i--
+        }
+      }
+      if (projectHeader.textContent === 'All todos') {
+        generateTable(getAllTodos(), incompleteTable, incompleteTBody)
+      } else {
+        generateTable(projects[project], incompleteTable, incompleteTBody)
+      }
     }
   }
 }
 
 // view all todos from all projects
 viewAllTodosBtn.addEventListener('click', () => {
-  generateTable(getAllTodos())
+  generateTable(getAllTodos(), incompleteTable, incompleteTBody)
   projectHeader.textContent = 'All todos'
 })
 
